f722abc67ac0ebec56a9121c6617faae9873d6ae
wintleh <hunter.wintle@gmail.com>
2020-07-17 19:36:57 -0400

    Add home page content

--- src/index.php
+++ src/index.php
@@ -10,7 +10,67 @@
 
 <body>
     <?php include "./templates/page-header.php" ?>
-    <section id="content">
+    <section id="content-wrapper">
+        <article id="welcome-wrapper">
+            <header>
+                <h1>Welcome to EKGViewer</h1>
+            </header>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+        </article>
+        <section id="page-info-container">
+            <article>
+                <header>
+                    <h2>History</h2>
+                </header>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    <br /><br />
+                    Learn more <a href="./history">here.</a>
+                </p>
+            </article>
+            <article>
+                <header>
+                    <h2>Definitions</h2>
+                </header>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    <br /><br />
+                    Learn more <a href="./definitions">here.</a>
+                </p>
+            </article>
+            <article>
+                <header>
+                    <h2>How To Place EKG Leads</h2>
+                </header>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    <br /><br />
+                    Learn more <a href="./how-to-place">here.</a>
+                </p>
+            </article>
+            <article>
+                <header>
+                    <h2>How To Interpret EKG Signals</h2>
+                </header>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    <br /><br />
+                    Learn more <a href="./how-to-interpret">here.</a>
+                </p>
+            </article>
+            <article>
+                <header>
+                    <h2>Quiz</h2>
+                </header>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    <br /><br />
+                    Test your knowledge <a href="./quiz">here.</a>
+                </p>
+            </article>
+        </section>
     </section>
     <?php include "./templates/page-footer.php" ?>
 </body>
