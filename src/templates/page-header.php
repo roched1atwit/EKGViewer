2a429f91e8def9cb2147150f64c7aa435fea05bf
wintleh <hunter.wintle@gmail.com>
2020-07-17 13:29:21 -0400

    Update header styling

--- src/templates/page-header.php
+++ src/templates/page-header.php
@@ -2,8 +2,9 @@
     <h1><a href="./">EKGViewer</a></h1>
     <nav id="header-nav">
         <a href="./history">History</a>
+        <a href="./definitions">Definitions</a>
         <a href="./how-to-place">How To Place EKG Leads</a>
         <a href="./how-to-interpret">How To Interpret EKG Signals</a>
-        <a href="./test">Test</a>
+        <a href="./quiz">Quiz</a>
     </nav>
 </header>
