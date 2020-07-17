b18216d5a122749983c1e475df75f2c7b00dafc1
wintleh <hunter.wintle@gmail.com>
2020-07-17 19:59:52 -0400

    Add starting html for all pages

---
+++ src/definitions.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>EKGViewer - Definitions</title>
+    <link href="./css/reset.css" rel="stylesheet" />
+    <link href="./templates/css/page-template.css" rel="stylesheet" />
+    <link href="./css/definitions.css" rel="stylesheet" />
+</head>
+
+<body>
+    <?php include "./templates/page-header.php" ?>
+    <section id="content-wrapper"></section>
+    <?php include "./templates/page-footer.php" ?>
+</body>
+
+</html>
