997793be899ed5bf5d5a541438219cfd3e029f96
wintleh <hunter.wintle@gmail.com>
2020-07-18 12:39:52 -0400

    Add content structure and styling

--- src/how-to-interpret.php
+++ src/how-to-interpret.php
@@ -10,7 +10,126 @@
 
 <body>
     <?php include "./templates/page-header.php" ?>
-    <section id="content-wrapper"></section>
+    <section id="content-wrapper">
+        <h1>SIGNAL CATEGORY</h1>
+        <section class="signal-type-wrapper">
+            <article>
+                <header>
+                    <h2>SIGNAL TYPE</h2>
+                </header>
+                <figure>
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/SinusRhythmLabels.svg/280px-SinusRhythmLabels.svg.png" alt="signal type name">
+                    <figcaption>
+                        EXPLAIN WAVE FORM
+                    </figcaption>
+                </figure>
+                <p>
+                    MORE INFO ABOUT SIGNAL
+                </p>
+            </article>
+            <article>
+                <header>
+                    <h2>SIGNAL TYPE</h2>
+                </header>
+                <figure>
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/SinusRhythmLabels.svg/280px-SinusRhythmLabels.svg.png" alt="signal type name">
+                    <figcaption>
+                        EXPLAIN WAVE FORM
+                    </figcaption>
+                </figure>
+                <p>
+                    MORE INFO ABOUT SIGNAL
+                </p>
+            </article>
+            <article>
+                <header>
+                    <h2>SIGNAL TYPE</h2>
+                </header>
+                <figure>
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/SinusRhythmLabels.svg/280px-SinusRhythmLabels.svg.png" alt="signal type name">
+                    <figcaption>
+                        EXPLAIN WAVE FORM
+                    </figcaption>
+                </figure>
+                <p>
+                    MORE INFO ABOUT SIGNAL
+                </p>
+            </article>
+            <article>
+                <header>
+                    <h2>SIGNAL TYPE</h2>
+                </header>
+                <figure>
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/SinusRhythmLabels.svg/280px-SinusRhythmLabels.svg.png" alt="signal type name">
+                    <figcaption>
+                        EXPLAIN WAVE FORM
+                    </figcaption>
+                </figure>
+                <p>
+                    MORE INFO ABOUT SIGNAL
+                </p>
+            </article>
+        </section>
+        <h1>SIGNAL CATEGORY</h1>
+        <section class="signal-type-wrapper">
+            <article>
+                <header>
+                    <h2>SIGNAL TYPE</h2>
+                </header>
+                <figure>
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/SinusRhythmLabels.svg/280px-SinusRhythmLabels.svg.png" alt="signal type name">
+                    <figcaption>
+                        EXPLAIN WAVE FORM
+                    </figcaption>
+                </figure>
+                <p>
+                    MORE INFO ABOUT SIGNAL
+                </p>
+            </article>
+            <article>
+                <header>
+                    <h2>SIGNAL TYPE</h2>
+                </header>
+                <figure>
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/SinusRhythmLabels.svg/280px-SinusRhythmLabels.svg.png" alt="signal type name">
+                    <figcaption>
+                        EXPLAIN WAVE FORM
+                    </figcaption>
+                </figure>
+                <p>
+                    MORE INFO ABOUT SIGNAL
+                </p>
+            </article>
+            <article>
+                <header>
+                    <h2>SIGNAL TYPE</h2>
+                </header>
+                <figure>
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/SinusRhythmLabels.svg/280px-SinusRhythmLabels.svg.png" alt="signal type name">
+                    <figcaption>
+                        EXPLAIN WAVE FORM
+                    </figcaption>
+                </figure>
+                <p>
+                    MORE INFO ABOUT SIGNAL
+                </p>
+            </article>
+            <article>
+                <header>
+                    <h2>SIGNAL TYPE</h2>
+                </header>
+                <figure>
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/SinusRhythmLabels.svg/280px-SinusRhythmLabels.svg.png" alt="signal type name">
+                    <figcaption>
+                        EXPLAIN WAVE FORM
+                    </figcaption>
+                </figure>
+                <p>
+                    MORE INFO ABOUT SIGNAL
+                </p>
+            </article>
+        </section>
+    </section>
     <?php include "./templates/page-footer.php" ?>
 </body>
 
