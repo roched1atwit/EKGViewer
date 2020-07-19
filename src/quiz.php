ce4098cb6220c21a3ae49a3eee0dcb1c5f51c0ea
wintleh <hunter.wintle@gmail.com>
2020-07-19 14:06:51 -0400

    Convert questions to classes

--- src/quiz.php
+++ src/quiz.php
@@ -1,3 +1,10 @@
+<?php
+include "./templates/quiz/text-answer-data.php";
+include "./templates/quiz/text-answer-question.php";
+include "./templates/quiz/image-answer-data.php";
+include "./templates/quiz/image-answer-question.php";
+?>
+
 <!DOCTYPE html>
 <html>
 
@@ -12,250 +19,88 @@
     <?php include "./templates/page-header.php" ?>
     <section id="content-wrapper">
         <form method="POST" action="">
-            <h1>Question 1 - HISTORY QUESTION</h1>
-            <section class="question-wrapper">
-                <div class="answer-wrapper">
-                    <input type="radio" name="question1" id="a1">
-                    <label for="a1">
-                        <article>
-                            <header>
-                                <h2>A</h2>
-                            </header>
-                            <p>
-                                ANSWER A
-                            </p>
-                        </article>
-                    </label>
-                </div>
-                <div class="answer-wrapper">
-                    <input type="radio" name="question1" id="b1">
-                    <label for="b1">
-                        <article>
-                            <header>
-                                <h2>B</h2>
-                            </header>
-                            <p>
-                                ANSWER B
-                            </p>
-                        </article>
-                    </label>
-                </div>
-                <div class="answer-wrapper">
-                    <input type="radio" name="question1" id="c1">
-                    <label for="c1">
-                        <article>
-                            <header>
-                                <h2>C</h2>
-                            </header>
-                            <p>
-                                ANSWER C
-                            </p>
-                        </article>
-                    </label>
-                </div>
-                <div class="answer-wrapper">
-                    <input type="radio" name="question1" id="d1">
-                    <label for="d1">
-                        <article>
-                            <header>
-                                <h2>D</h2>
-                            </header>
-                            <p>
-                                ANSWER D
-                            </p>
-                        </article>
-                    </label>
-                </div>
-            </section>
-            <h1>Question 2 - DEFINITION QUESTION</h1>
-            <section class="question-wrapper">
-                <div class="answer-wrapper">
-                    <input type="radio" name="question2" id="a2">
-                    <label for="a2">
-                        <article>
-                            <header>
-                                <h2>A</h2>
-                            </header>
-                            <p>
-                                ANSWER A
-                            </p>
-                        </article>
-                    </label>
-                </div>
-                <div class="answer-wrapper">
-                    <input type="radio" name="question2" id="b2">
-                    <label for="b2">
-                        <article>
-                            <header>
-                                <h2>B</h2>
-                            </header>
-                            <p>
-                                ANSWER B
-                            </p>
-                        </article>
-                    </label>
-                </div>
-                <div class="answer-wrapper">
-                    <input type="radio" name="question2" id="c2">
-                    <label for="c2">
-                        <article>
-                            <header>
-                                <h2>C</h2>
-                            </header>
-                            <p>
-                                ANSWER C
-                            </p>
-                        </article>
-                    </label>
-                </div>
-                <div class="answer-wrapper">
-                    <input type="radio" name="question2" id="d2">
-                    <label for="d2">
-                        <article>
-                            <header>
-                                <h2>D</h2>
-                            </header>
-                            <p>
-                                ANSWER D
-                            </p>
-                        </article>
-                    </label>
-                </div>
-            </section>
-            <h1>Question 3 - HOW TO PLACE QUESTION</h1>
-            <section class="question-wrapper">
-                <div class="answer-wrapper">
-                    <input type="radio" name="question3" id="a3">
-                    <label for="a3">
-                        <article>
-                            <header>
-                                <h2>A</h2>
-                            </header>
-                            <figure>
-                                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/SinusRhythmLabels.svg/280px-SinusRhythmLabels.svg.png" alt="signal type name">
-                                <figcaption>
-                                    EXPLAIN WAVE FORM
-                                </figcaption>
-                            </figure>
-                        </article>
-                    </label>
-                </div>
-                <div class="answer-wrapper">
-                    <input type="radio" name="question3" id="b3">
-                    <label for="b3">
-                        <article>
-                            <header>
-                                <h2>B</h2>
-                            </header>
-                            <figure>
-                                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/SinusRhythmLabels.svg/280px-SinusRhythmLabels.svg.png" alt="signal type name">
-                                <figcaption>
-                                    EXPLAIN WAVE FORM
-                                </figcaption>
-                            </figure>
-                        </article>
-                    </label>
-                </div>
-                <div class="answer-wrapper">
-                    <input type="radio" name="question3" id="c3">
-                    <label for="c3">
-                        <article>
-                            <header>
-                                <h2>C</h2>
-                            </header>
-                            <figure>
-                                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/SinusRhythmLabels.svg/280px-SinusRhythmLabels.svg.png" alt="signal type name">
-                                <figcaption>
-                                    EXPLAIN WAVE FORM
-                                </figcaption>
-                            </figure>
-                        </article>
-                    </label>
-                </div>
-                <div class="answer-wrapper">
-                    <input type="radio" name="question3" id="d3">
-                    <label for="d3">
-                        <article>
-                            <header>
-                                <h2>D</h2>
-                            </header>
-                            <figure>
-                                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/SinusRhythmLabels.svg/280px-SinusRhythmLabels.svg.png" alt="signal type name">
-                                <figcaption>
-                                    EXPLAIN WAVE FORM
-                                </figcaption>
-                            </figure>
-                        </article>
-                    </label>
-                </div>
-            </section>
-            <h1>Question 4 - Which is an example of WAVE FORM?</h1>
-            <section class="question-wrapper">
-                <div class="answer-wrapper">
-                    <input type="radio" name="question4" id="a4">
-                    <label for="a4">
-                        <article>
-                            <header>
-                                <h2>A</h2>
-                            </header>
-                            <figure>
-                                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/SinusRhythmLabels.svg/280px-SinusRhythmLabels.svg.png" alt="signal type name">
-                                <figcaption>
-                                    EXPLAIN WAVE FORM
-                                </figcaption>
-                            </figure>
-                        </article>
-                    </label>
-                </div>
-                <div class="answer-wrapper">
-                    <input type="radio" name="question4" id="b4">
-                    <label for="b4">
-                        <article>
-                            <header>
-                                <h2>B</h2>
-                            </header>
-                            <figure>
-                                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/SinusRhythmLabels.svg/280px-SinusRhythmLabels.svg.png" alt="signal type name">
-                                <figcaption>
-                                    EXPLAIN WAVE FORM
-                                </figcaption>
-                            </figure>
-                        </article>
-                    </label>
-                </div>
-                <div class="answer-wrapper">
-                    <input type="radio" name="question4" id="c4">
-                    <label for="c4">
-                        <article>
-                            <header>
-                                <h2>C</h2>
-                            </header>
-                            <figure>
-                                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/SinusRhythmLabels.svg/280px-SinusRhythmLabels.svg.png" alt="signal type name">
-                                <figcaption>
-                                    EXPLAIN WAVE FORM
-                                </figcaption>
-                            </figure>
-                        </article>
-                    </label>
-                </div>
-                <div class="answer-wrapper">
-                    <input type="radio" name="question4" id="d4">
-                    <label for="d4">
-                        <article>
-                            <header>
-                                <h2>D</h2>
-                            </header>
-                            <figure>
-                                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/SinusRhythmLabels.svg/280px-SinusRhythmLabels.svg.png" alt="signal type name">
-                                <figcaption>
-                                    EXPLAIN WAVE FORM
-                                </figcaption>
-                            </figure>
-                        </article>
-                    </label>
-                </div>
-            </section>
+            <?php
+            (new TextAnswerQuestion(
+                "1",
+                "History Question",
+                array(
+                    new TextAnswerData("A", "ANSWER A"),
+                    new TextAnswerData("B", "ANSWER B"),
+                    new TextAnswerData("C", "ANSWER C"),
+                    new TextAnswerData("D", "ANSWER D")
+                )
+            ))->output();
+            (new TextAnswerQuestion(
+                "2",
+                "Definition Question",
+                array(
+                    new TextAnswerData("A", "ANSWER A"),
+                    new TextAnswerData("B", "ANSWER B"),
+                    new TextAnswerData("C", "ANSWER C"),
+                    new TextAnswerData("D", "ANSWER D")
+                )
+            ))->output();
+            (new ImageAnswerQuestion(
+                "3",
+                "How To Place Question",
+                array(
+                    new ImageAnswerData(
+                        "A",
+                        "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/SinusRhythmLabels.svg/280px-SinusRhythmLabels.svg.png",
+                        "IMG ALT",
+                        "EXPLAIN WAVE FORM"
+                    ),
+                    new ImageAnswerData(
+                        "B",
+                        "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/SinusRhythmLabels.svg/280px-SinusRhythmLabels.svg.png",
+                        "IMG ALT",
+                        "EXPLAIN WAVE FORM"
+                    ),
+                    new ImageAnswerData(
+                        "C",
+                        "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/SinusRhythmLabels.svg/280px-SinusRhythmLabels.svg.png",
+                        "IMG ALT",
+                        "EXPLAIN WAVE FORM"
+                    ),
+                    new ImageAnswerData(
+                        "D",
+                        "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/SinusRhythmLabels.svg/280px-SinusRhythmLabels.svg.png",
+                        "IMG ALT",
+                        "EXPLAIN WAVE FORM"
+                    )
+                )
+            ))->output();
+            (new ImageAnswerQuestion(
+                "4",
+                "Which is an example of WAVE FORM?",
+                array(
+                    new ImageAnswerData(
+                        "A",
+                        "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/SinusRhythmLabels.svg/280px-SinusRhythmLabels.svg.png",
+                        "IMG ALT",
+                        "EXPLAIN WAVE FORM"
+                    ),
+                    new ImageAnswerData(
+                        "B",
+                        "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/SinusRhythmLabels.svg/280px-SinusRhythmLabels.svg.png",
+                        "IMG ALT",
+                        "EXPLAIN WAVE FORM"
+                    ),
+                    new ImageAnswerData(
+                        "C",
+                        "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/SinusRhythmLabels.svg/280px-SinusRhythmLabels.svg.png",
+                        "IMG ALT",
+                        "EXPLAIN WAVE FORM"
+                    ),
+                    new ImageAnswerData(
+                        "D",
+                        "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/SinusRhythmLabels.svg/280px-SinusRhythmLabels.svg.png",
+                        "IMG ALT",
+                        "EXPLAIN WAVE FORM"
+                    )
+                )
+            ))->output();
+            ?>
             <input type="submit" value="Submit" class="form-button">
             <input type="reset" value="Clear" class="form-button">
         </form>
---
+++ src/templates/quiz/image-answer-question.php
@@ -0,0 +1,39 @@
+<?php
+class ImageAnswerQuestion
+{
+    private $_questionNum;
+    private $_questionText;
+    private $_answers;
+
+    function __construct($questionNum, $questionText, $answers)
+    {
+        $this->_questionNum = $questionNum;
+        $this->_questionText = $questionText;
+        $this->_answers = $answers;
+    }
+
+    public function output()
+    {
+        echo "<h1>Question " . $this->_questionNum . " - " . $this->_questionText . "</h1>";
+        echo "<section class='question-wrapper'>";
+        for ($i = 0; $i < count($this->_answers); $i++) 
+        {
+            echo "<div class='answer-wrapper'>";
+            echo "<input type='radio' name='question" . $this->_questionNum . "' id='" . $this->_questionNum . $i . "'>";
+            echo "<label for='" . $this->_questionNum . $i . "'>";
+            echo "<article>";
+            echo "<header>";
+            echo "<h2>" . $this->_answers[$i]->getHeaderText() . "</h2>";
+            echo "</header>";
+            echo "<figure>";
+            echo "<img src='" . $this->_answers[$i]->getImgPath() . "' alt='" . $this->_answers[$i]->getImgAlt() . "'>";
+            echo "<figcaption>" . $this->_answers[$i]->getAnswerText() . "</figcaption>";
+            echo "</figure>";
+            echo "</article>";
+            echo "</label>";
+            echo "</div>";
+        }
+        echo "</section>";
+    }
+}
+?>
