8b8efdd53bdd46ddc4bab25d1ebf71257d9ebf53
am2021 <58558673+am2021@users.noreply.github.com>
2020-07-27 14:35:53 -0400

    basic history page

--- src/history.php
+++ src/history.php
@@ -10,7 +10,22 @@
 
 <body>
     <?php include "./templates/page-header.php" ?>
-    <section id="content-wrapper"></section>
+    <section id="content-wrapper">
+        <h1>History</h1>
+        <section class="history">
+            <div>
+                <ul>
+                    <li><b>Late 1700's</b> - the first step in the development of the modern electrocardiograph machine was the creation of the machine that could sense, electric current. This was called the a galvanometer.</li>
+                    <li><b>1786</b> - Dr. Luigi Galvan, an Italian physician, discovered that skeletal muslces worled by producing electric current.</li>
+                    <li><b>1842</b> - Dr. Carlo Matteucci discovered that there is a electrical current that comes with each heart beat in a frog.</li>
+                    <li><b>Mid 1800's</b> - The Rheotome, a machine to measure electrical current, was inveted.</li>
+                    <li><b>1872</b> - Gabrilled Lippman devised the capillary electrometer, an improved Rheotome. August Waller was able to record the first human electrocardiogram.</li>
+                    <li><b>1893</b> - Dr. Wilhelm Einthoven refined the capillary electrometer to show five deflections in the elecctrical current passing through the heart. The term "electrocardiogram" is used for the first time.</li>
+                    <li><b>1901</b> - Dr. Eintoven develops the string galvanometer. He was awared the Nobel Prize in Medicine and Physiology for the electrocardiograph</li>
+                </ul>
+            </div>
+        </section>
+    </section>
     <?php include "./templates/page-footer.php" ?>
 </body>
 
