06ffe74eff8c53517b257cebb5c9c420cd6561f6
Dan J. Roche <roched1@wit.edu>
2020-07-29 03:41:37 -0400

    added info on preparing patient

--- src/how-to-place.php
+++ src/how-to-place.php
@@ -10,7 +10,83 @@
 
 <body>
     <?php include "./templates/page-header.php" ?>
-    <section id="content-wrapper"></section>
+    <section id="content-wrapper">
+        <article id="title-wrapper">
+            <header>
+                <h1>How to Apply an EKG to a Patient</h1>
+            </header>
+        </article>
+        <section id="page-info-container">
+            <article>
+                <header>
+                    <h2>Prepare The Patient</h2>
+                </header>
+                <p>
+                    <p> 
+                        The first step in preparing a patient for EKG monitoring is
+                        to prep the patient. Some of the steps listed below may seem
+                        obvious or straight forward, but they nevertheless are important.
+                    </p>                
+                    <ul>
+                        <li>
+                            <b>Confirm the patient's identity.</b> This step is crucial to any
+                            medical procedure. Make sure you have the correct patient
+                            before administering the EKG.
+                        </li>
+                        <li>
+                            <b>Explain the procedure to the patient.</b> This is essential
+                            regardless of the medical treatment you are going to provide. 
+                            Every pateint should be able to make informed decisions 
+                            regarding their medical treatment. It is your job to make 
+                            sure they understand the goal of any proposed treatment as well 
+                            as the risks associated with it. This is also a great opportunity
+                            to establish a rapport with your patient to help them feel 
+                            comfortable working with you.
+                        </li>
+                        <li>
+                            <b>Finally, obtain patient consent. </b>Now that you have fully explained
+                            the procedure to the patient, the last step is to gain their
+                            permission to administer the EKG.
+                        </li>
+                        <li>
+                            <b>Set the room to a comfortably warm temperature.</b> This will prevent 
+                            muscular tension or movements which will produce artifacts on the EKG
+                            recording.
+                        </li>
+                        <li>
+                            <b>Give the patient privacy.</b> Their chest will be exposed to place the EKG
+                            leads. So close the door to their room or draw the curtains around
+                            their bed in order to respect their privacy.
+                        <li>
+                            <b>Remove any jewelry.</b> The metal can interfere with the electric signals in the 
+                            EKG and create artifiacts on the EKG recording.
+                        </li>
+                        <li>
+                            <b>Wash your hands.</b>
+                        </li>
+                        <li><b>Place the patient in a relaxed, supine position.</b> A supine position is
+                            the opposite of prone, meaning the patient is lying flat on their back with their
+                            head and chest facing upwards towards the ceiling. All limbs should be supported
+                            and their chest should be exposed. No muscular work should be required by the 
+                            patient to maintain their position and posture.
+                        </li>
+                        <li><b>Prepare the patient's skin.</b> Their chest should be cleaned with soap and
+                            water. Additionally, if the patient has chest hair, it should be removed before
+                            the EKG leads are applied. The goal is to have the leads placed cleanly on the skin 
+                            with no other debris in between.
+                        </li>
+                    </ul>
+                    <p>
+                        After you have completed each one of these steps, you may begin to administer the EKG to 
+                        the patient.
+                    </p>
+                </p>
+            </article>
+            <article>
+                hey
+            </article>
+        </section>
+    </section>
     <?php include "./templates/page-footer.php" ?>
 </body>
 
